218eab92e8b9ce716b3364b14025cb99fed911d6
Durai <durai@Durais-MBP.fios-router.home>
2020-06-14 23:35:17 -0400

    A simple in memory password handler is added

--- src/main/java/com/bindinfo/imdb/config/BasicConfiguration.java
+++ src/main/java/com/bindinfo/imdb/config/BasicConfiguration.java
@@ -13,6 +13,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -21,5 +36,4 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
-
 }
